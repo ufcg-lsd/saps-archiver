a53865dc22607bae2659588be717c01b0ddc4c7d
Cilas Marques <cilas.marques@ccc.ufcg.edu.br>
2022-12-14 16:24:56 -0300

    Updating some script variables

--- scripts/build_archiver_overview.sh
+++ scripts/build_archiver_overview.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 DIRNAME=`dirname $0`
-cd $DIRNAME/..
-SCRIPT_PATH=`pwd`/scripts
+cd $DIRNAME/.
 
-echo "task_id,inputdownloading,preprocessing,processing" > $SCRIPT_PATH/archived_overview_data.csv 
+echo "task_id,inputdownloading,preprocessing,processing" > ./archived_overview_data.csv 
 
 for dir in /nfs/archiver/*
 do
@@ -16,20 +15,25 @@ do
 	do
 		#inputdownloading/ preprocessing/ processing/ 
 		if [ $subdir == ${dir}/inputdownloading ]; then	
-			inputdownlaoding=`sudo ls $subdir | wc -l`
-                	#echo $inputdownlaoding
+      inputdownlaoding=`sudo ls $subdir | wc -l`
+      #echo $inputdownlaoding
 		elif [ $subdir == ${dir}/preprocessing ]; then 
-                        preprocessing=`sudo ls $subdir | wc -l`
+      preprocessing=`sudo ls $subdir | wc -l`
 			#echo $preprocessing
-               	elif [ $subdir == ${dir}/processing ]; then
-                        processing=`sudo ls $subdir | wc -l`
+    elif [ $subdir == ${dir}/processing ]; then
+      processing=`sudo ls $subdir | wc -l`
 			#echo $processing
-                fi
+    fi
 	done
 
 	task_id=`echo $dir | awk -F'/' '{print $4}'`
-	echo $task_id,$inputdownlaoding,$preprocessing,$processing >> $SCRIPT_PATH/archived_overview_data.csv 
+	echo $task_id,$inputdownlaoding,$preprocessing,$processing >> ./archived_overview_data.csv 
 done
 
-# Fill and uncoment the following command
-# scp -i <key_path> $SCRIPT_PATH/archived_overview_data.csv ubuntu@<dispatcher_ip>:<dispatcher_stats_path>
+# Update these fields 
+ssh_key_file="/home/ubuntu/.ssh/saps22"
+remote_user="ubuntu"
+dispatcher_ip="127.0.0.1"
+dispatcher_stats_path="/home/ubuntu/saps-dispatcher/stats/archived_overview_data.csv"
+
+scp -i $ssh_key_file ./archived_overview_data.csv $remote_user@$dispatcher_ip:$dispatcher_stats_path
