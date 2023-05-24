396367c37ffff81800350f600b44930c26fb99b4
João Henrique Almeida Xavier <Jhenriquealmeidaxavier@hotmail.com>
2023-05-24 15:38:51 -0300

    Refactor

--- src/main/java/saps/archiver/core/DefaultPermanentStorage.java
+++ src/main/java/saps/archiver/core/DefaultPermanentStorage.java
@@ -7,7 +7,6 @@ import static saps.common.core.storage.PermanentStorageConstants.PROCESSING_DIR;
 import static saps.common.core.storage.PermanentStorageConstants.SAPS_TASK_STAGE_DIR_PATTERN;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import org.apache.commons.io.FileUtils;
@@ -17,24 +16,20 @@ import saps.archiver.interfaces.PermanentStorage;
 import saps.common.core.storage.exceptions.InvalidPropertyException;
 import saps.common.utils.SapsPropertiesConstants;
 
+public class FSPermanentStorage implements PermanentStorage {
 
-public class DefaultPermanentStorage implements PermanentStorage {
-
-  public static final Logger LOGGER = Logger.getLogger(DefaultPermanentStorage.class);
+  public static final Logger LOGGER = Logger.getLogger(FSPermanentStorage.class);
   public static final String NFS_STORAGE_TASK_DIR_PATTERN ="%s" + File.separator + "%s" + File.separator + "%s";
   public static final String NFS_STORAGE_TASK_URL_PATTERN = "%s" + File.separator + "%s";
-
+  
   private final String nfsTempStoragePath;
   private final String nfsPermanentStoragePath;
   private final String tasksDirName;
- 
 
-  public DefaultPermanentStorage(Properties properties) throws InvalidPropertyException {
-  
+  public FSPermanentStorage(Properties properties) throws InvalidPropertyException {
     this.nfsTempStoragePath = properties.getProperty(SapsPropertiesConstants.SAPS_TEMP_STORAGE_PATH);
     this.nfsPermanentStoragePath = properties.getProperty(SapsPropertiesConstants.NFS_PERMANENT_STORAGE_PATH);
     this.tasksDirName = properties.getProperty(SapsPropertiesConstants.PERMANENT_STORAGE_TASKS_DIR); 
-
   }
 
   @Override
@@ -52,11 +47,9 @@ public class DefaultPermanentStorage implements PermanentStorage {
       String taskId = task.getTaskId();
       LOGGER.info("Archiving task [" + taskId + "] to permanent storage.");
       String nfsTaskDirPath = createTaskDir(tasksDirName, taskId);
-  
-      // Define the list of directories to copy
+ 
       String[] dirsToCopy = {INPUTDOWNLOADING_DIR, PREPROCESSING_DIR, PROCESSING_DIR};
-      
-      // Copy each directory in the list to the NFS task directory
+
       for (String dir : dirsToCopy) {
           String localDir = String.format(SAPS_TASK_STAGE_DIR_PATTERN, nfsTempStoragePath, taskId, dir);
           copyDirToDir(localDir, nfsTaskDirPath);
@@ -64,42 +57,14 @@ public class DefaultPermanentStorage implements PermanentStorage {
       return true;
   }
   
-
   private String createTaskDir(String tasksDir, String taskId) throws IOException {
-    File storageDir = new File(nfsPermanentStoragePath);
-    
-    if (!storageDir.exists()) {
-      throw new FileNotFoundException(
-          "The nfs storage directory [" + nfsPermanentStoragePath + "] was not found");
-    }
     File nfsTaskDir = new File(String.format(NFS_STORAGE_TASK_DIR_PATTERN, nfsPermanentStoragePath, tasksDir, taskId));
-    FileUtils.forceMkdir(nfsTaskDir);
+    nfsTaskDir.mkdir();
     return nfsTaskDir.getAbsolutePath();
   }
-
 
   private void copyDirToDir(String src, String dest) throws IOException {
     LOGGER.debug("Copying [" + src + "] into [" + dest + "]");
     FileUtils.copyDirectoryToDirectory(new File(src), new File(dest));
   }
-
 }
-
-
-
-
-
-
-
-
-
-/*
-*Retirei o método CheckPropeties, pois as propriedades definidas era para verificar se algum
-* tipo de armazenamento havia sido selecionado (SWIFT/NFS) como usaremos apenas nfs, não tem
-* necessidade dessa validação.
-*/
-
-/*
- * O debugMode, serva para armazenar o output de tasks que falharam, nesse caso optei por
- * tirar essa opção, não tera DebugMode, ela será apenas excluida
- */
