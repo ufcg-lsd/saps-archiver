f3c5db9d3960a401693fa9ecb66415eded290d6d
João Henrique Almeida Xavier <63925427+jhenriqueax@users.noreply.github.com>
2023-06-29 09:23:30 -0300

    Rename to FS

--- src/main/java/saps/archiver/core/DefaultPermanentStorage.java
+++ src/main/java/saps/archiver/core/DefaultPermanentStorage.java
@@ -19,23 +19,23 @@ import saps.common.utils.SapsPropertiesConstants;
 public class FSPermanentStorage implements PermanentStorage {
 
   public static final Logger LOGGER = Logger.getLogger(FSPermanentStorage.class);
-  public static final String NFS_STORAGE_TASK_DIR_PATTERN ="%s" + File.separator + "%s" + File.separator + "%s";
-  public static final String NFS_STORAGE_TASK_URL_PATTERN = "%s" + File.separator + "%s";
+  public static final String FS_STORAGE_TASK_DIR_PATTERN ="%s" + File.separator + "%s" + File.separator + "%s";
+  public static final String FS_STORAGE_TASK_URL_PATTERN = "%s" + File.separator + "%s";
   
-  private final String nfsTempStoragePath;
-  private final String nfsPermanentStoragePath;
+  private final String FSTempStoragePath;
+  private final String FSPermanentStoragePath;
   private final String tasksDirName;
 
   public FSPermanentStorage(Properties properties) throws InvalidPropertyException {
-    this.nfsTempStoragePath = properties.getProperty(SapsPropertiesConstants.SAPS_TEMP_STORAGE_PATH);
-    this.nfsPermanentStoragePath = properties.getProperty(SapsPropertiesConstants.NFS_PERMANENT_STORAGE_PATH);
+    this.FSTempStoragePath = properties.getProperty(SapsPropertiesConstants.SAPS_TEMP_STORAGE_PATH);
+    this.FSPermanentStoragePath = properties.getProperty(SapsPropertiesConstants.FS_PERMANENT_STORAGE_PATH);
     this.tasksDirName = properties.getProperty(SapsPropertiesConstants.PERMANENT_STORAGE_TASKS_DIR); 
   }
 
   @Override
   public String delete(SapsImage task) throws IOException {
     String taskId = task.getTaskId();
-    String taskDirPath = String.format(NFS_STORAGE_TASK_DIR_PATTERN, nfsPermanentStoragePath, tasksDirName, task.getTaskId());
+    String taskDirPath = String.format(FS_STORAGE_TASK_DIR_PATTERN, FSPermanentStoragePath, tasksDirName, task.getTaskId());
     File taskDir = new File(taskDirPath);
     FileUtils.deleteDirectory(taskDir);
     
@@ -46,21 +46,21 @@ public class FSPermanentStorage implements PermanentStorage {
   public boolean archive(SapsImage task) throws IOException {
       String taskId = task.getTaskId();
       LOGGER.info("Archiving task [" + taskId + "] to permanent storage.");
-      String nfsTaskDirPath = createTaskDir(tasksDirName, taskId);
+      String FSTaskDirPath = createTaskDir(tasksDirName, taskId);
  
       String[] dirsToCopy = {INPUTDOWNLOADING_DIR, PREPROCESSING_DIR, PROCESSING_DIR};
 
       for (String dir : dirsToCopy) {
-          String localDir = String.format(SAPS_TASK_STAGE_DIR_PATTERN, nfsTempStoragePath, taskId, dir);
-          copyDirToDir(localDir, nfsTaskDirPath);
+          String localDir = String.format(SAPS_TASK_STAGE_DIR_PATTERN, FSTempStoragePath, taskId, dir);
+          copyDirToDir(localDir, FSTaskDirPath);
       }
       return true;
   }
   
   private String createTaskDir(String tasksDir, String taskId) throws IOException {
-    File nfsTaskDir = new File(String.format(NFS_STORAGE_TASK_DIR_PATTERN, nfsPermanentStoragePath, tasksDir, taskId));
-    nfsTaskDir.mkdir();
-    return nfsTaskDir.getAbsolutePath();
+    File FSTaskDir = new File(String.format(FS_STORAGE_TASK_DIR_PATTERN, FSPermanentStoragePath, tasksDir, taskId));
+    FSTaskDir.mkdir();
+    return FSTaskDir.getAbsolutePath();
   }
 
   private void copyDirToDir(String src, String dest) throws IOException {
